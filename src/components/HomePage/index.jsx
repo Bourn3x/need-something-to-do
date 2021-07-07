38422eb7fabea60587cd0d42bd515110a7c28f74
Julian Chong <julianchong10@gmail.com>
2021-07-07 21:10:04 +0800

    🎨 UI: added main background color

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./HomePage.module.scss"
+import Title from "../Title"
+
+const HomePage = () => { 
+  return(
+    <div className={`${styles['main-container']}`}>
+    </div>
+  )
+}
+
+export default HomePage
