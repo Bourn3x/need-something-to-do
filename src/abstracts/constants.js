8d461943a8f6439b37635365bb7e8f6ffc64053d
Julian Chong <julianchong10@gmail.com>
2021-07-10 19:08:06 +0800

    :art: UI: added animation for laptop svg

---
+++ src/abstracts/constants.js
@@ -0,0 +1,2 @@
+export const SVG_DRAW_DELAY = 8000 // in ms
+export const SVG_DRAW_DURATION = 300 // in frames
--- src/components/Laptop/index.jsx
+++ src/components/Laptop/index.jsx
@@ -2,10 +2,18 @@ import  { ReactComponent as ReactLogo } from "./laptop.svg"
 import "./Laptop.scss"
 import { useEffect } from "react"
 import Vivus from "vivus"
+import { SVG_DRAW_DELAY, SVG_DRAW_DURATION } from "../../abstracts/constants"
 
 const Laptop = () => {
   useEffect(() => {
-    new Vivus('laptop', { duration: 200 });
+    const svg = new Vivus('laptop', { 
+      duration: SVG_DRAW_DURATION,
+    }).stop()
+
+    setTimeout(() => {
+      svg.play()
+    }, 
+    SVG_DRAW_DELAY);
   })
 
   return (
